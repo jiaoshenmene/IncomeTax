b237476d2ed659962b621fb259168a2b431d0d46
杜甲 <dujia211294@sogou-inc.com>
2018-03-05 14:53:33 +0800

    Initial Commit

---
+++ IncomeTax/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  IncomeTax
+//
+//  Created by 杜甲 on 2018/3/5.
+//  Copyright © 2018年 杜甲. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
